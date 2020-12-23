5dddfba3505d9789530b670ea958490b71486ac7
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2020-12-23 21:26:37 +0530

    Change base url

--- src/constants/BaseURL.js
+++ src/constants/BaseURL.js
@@ -1,4 +1,4 @@
-exports.BASE_API = "http://localhost:3000"
-// exports.BASE_API = "https://easylife123.herokuapp.com"
+// exports.BASE_API = "http://localhost:3000"
+exports.BASE_API = "https://easylife123.herokuapp.com"
 
 exports.FETCH_ALL_PRICES_URL = `${this.BASE_API}/info/get_all_prices`
